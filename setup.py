f25b843d426c18ff17e2fa8cc93e2decffd2ade5
Kevin Frommelt <kevin.frommelt@webfilings.com>
2014-10-10 13:57:33 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
-import os, time
 from setuptools import find_packages, setup
 from pip.req import parse_requirements
+
 
 def get_requirements(filename):
     reqs = parse_requirements(filename)
     return [str(r.req) for r in reqs]
 
+
 def get_install_requires():
     return get_requirements('requirements.txt')
 
-def get_test_requires():
-    return get_requirements('requirements_dev.txt')
 
 setup_args = dict(
     name='pies',
@@ -18,7 +17,6 @@ setup_args = dict(
     packages=find_packages(),
     namespace_packages=['pies'],
     install_requires=get_install_requires(),
-    tests_require=get_test_requires(),
     entry_points={
         'console_scripts': [
             'pies-test=pies.util:say_hello'
@@ -27,9 +25,4 @@ setup_args = dict(
 )
 
 if __name__ == '__main__':
-    if os.environ.get('EAT_PIES_SLOWLY'):
-        len = float(os.getenv('EAT_PIES_SLOWLY', 10))
-        print "Going to have a nice little nap for {0} seconds".format(len)
-        time.sleep(len)
-
     setup(**setup_args)
