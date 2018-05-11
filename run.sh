516e667d81eada6d63a6dd0fb53ab71bcbd53b6d
Kevin Frommelt <kevin.frommelt@workiva.com>
2018-05-11 11:21:14 -0500

    Report signals

--- run.sh
+++ run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+trap "echo 'Received SIGINT'; exit" INT
+trap "echo 'Received SIGTERM'; exit" TERM
+trap "echo 'Received SIGERR'; exit" ERR
+trap "kill 0" EXIT
+echo "pid is $$"
+
 function log {
 while [ 1 ]   # Endless loop.
 do
   echo "Log stuff"
-  sleep 30
+  sleep 5
 done
 }
 
 log &
 
-/usr/sbin/nginx
+/usr/sbin/nginx &
+PID="$!"
+
+wait $PID
